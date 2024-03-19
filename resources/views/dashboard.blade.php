f425a8d14eed4952ba5f29c6e1339564ffd6c815
abdalla-mans <a.mansour.code@gmail.com>
2024-03-19 03:47:10 +0200

    my test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,6 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-
-        <div>my div</div>
     </x-slot>
 
     <div class="py-12">
